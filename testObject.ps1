896f285ba512f024b69d49f9870c7fd33cfe8699
chanmmn <33271651+chanmmn@users.noreply.github.com>
2022-09-16 11:24:09 +0800

    Commit Create Object
    Commit Create Object

---
+++ testObject.ps1
@@ -0,0 +1,20 @@
+﻿$myObject = [PSCustomObject]@{
+    Name     = 'Kevin'
+    Language = 'PowerShell'
+    State    = 'Texas'
+}
+
+Write-Output  $myObject.Name
+#$myObject.Name = 'John'
+#Write-Output  $myObject[1].Name
+#[PSCustomObject[]]$ia
+$b = (New-Object 'object[]' 2)
+$b[0] = $myObject
+$myObject1 = [PSCustomObject]@{
+    Name     = 'Chan'
+    Language = 'PowerShell'
+    State    = 'Texas'
+}
+$b[1] = $myObject1
+Write-Output  $b[0].Name
+Write-Output  $b[1].Name
