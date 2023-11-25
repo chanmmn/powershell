80d8eb4e8e3d8408cb9b997c01a0daa31e48b318
chanmmn <33271651+chanmmn@users.noreply.github.com>
2023-11-25 11:23:52 +0800

    Commit StartStopAureAppService
    Commit StartStopAureAppService

---
+++ 2023/StartStopAzureAppService.ps1
@@ -0,0 +1,17 @@
+﻿# Install and import the Az module
+Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
+Import-Module Az -Force
+
+# Connect to Azure (sign in if not already signed in)
+Connect-AzAccount
+
+# Set variables for your Azure subscription and resource group
+$subscriptionId = "<YourSubscriptionId>"
+$resourceGroupName = "<YourResourceGroupName>"
+$appServiceName = "<YourAppServiceName>"
+
+# Select the target subscription
+Set-AzContext -SubscriptionId $subscriptionId
+
+# Start the Azure App Service
+Start-AzWebApp -ResourceGroupName $resourceGroupName -Name $appServiceName
