cf671f152db379cd78671ed8d4716def2bc9eb25
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-04-27 01:06:42 -0700

    Create AzureStorageFilterDate.ps1
    cimmit AzureStorageFilterDate

---
+++ 2024/AzureStorageFilterDate/AzureStorageFilterDate.ps1
@@ -0,0 +1,7 @@
+﻿Connect-AzAccount -Tenant TenantId -Subscription SubscriptionId 
+$resourceGroupName =" resourceGroupName "  
+$storageAccName =" storageAccName "  
+$directoryPath ="document" 
+$container = "container"
+$ctx=(Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccName).Context  
+Get-AzStorageBlob -Container "document" -Context $ctx  | Where-Object {$_.LastModified -gt "2024-03-15"} 
