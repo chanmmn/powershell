8cbd339580763d2efa9c136e02f1cd036a511b8b
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-05-09 08:35:23 -0700

    sss
    ssss

---
+++ 2024/AzureStorageGetFile/Getfilesv3.ps1
@@ -0,0 +1,21 @@
+﻿
+
+Connect-AzAccount -Tenant TanantId -Subscription SubscriptionId
+
+$resourceGroupName="resourcename"  
+$storageAccName="accname"  
+$directoryPath="document" 
+$container = "container"
+
+Function Getfiles
+{
+    $ctx=(Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccName).Context  
+    $files = Get-AzStorageBlob -Container "document" -Context $ctx  | Where-Object {$_.LastModified -gt "2023-12-04" -and $_.LastModified -lt "2023-12-05"}
+    foreach ($file in $files)  
+        {  
+            write-host -ForegroundColor Yellow $file.Name 
+            Get-AzStorageBlobContent -Container "document" -Context $ctx  -Blob $file.Name -Destination "C:\test\test" 
+        } 
+}
+
+Getfiles
