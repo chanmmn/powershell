167238505f570f5ed902b15d6893760b8db10d41
chanmmn <33271651+chanmmn@users.noreply.github.com>
2026-01-27 15:46:19 +0800

    Commit remove AOMEI

---
+++ 2026/aomeiremove/aomei.ps1
@@ -0,0 +1,20 @@
+﻿# Run PowerShell as Administrator
+
+# Get current system PATH
+$oldPath = [Environment]::GetEnvironmentVariable('Path', 'Machine')
+
+# Remove all AOMEI references
+$newPath = ($oldPath -split ';' | Where-Object {
+    $_ -notmatch 'AOMEI' -and 
+    $_ -notmatch '\\AOMEI Backupper\\'
+}) -join ';'
+
+# Update PATH
+[Environment]::SetEnvironmentVariable('Path', $newPath, 'Machine')
+
+# Also clean user PATH
+$oldUserPath = [Environment]::GetEnvironmentVariable('Path', 'User')
+$newUserPath = ($oldUserPath -split ';' | Where-Object {
+    $_ -notmatch 'AOMEI'
+}) -join ';'
+[Environment]::SetEnvironmentVariable('Path', $newUserPath, 'User')
